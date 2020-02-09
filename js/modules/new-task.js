574ec96636fcef1415814298df7f9d40433b4ba3
Lukasz Lieske <lukasz.lieske@gmail.com>
2020-02-09 16:01:46 +0000

    finishing

---
+++ js/modules/new-task.js
@@ -0,0 +1,53 @@
+function addNewTask(title) {
+    var taskLi = document.createElement('li');
+
+    taskLi.classList.add('single-task');
+    taskLi.innerHTML = prepareTaskHTML(title);
+
+    // Events - toggle and delte
+    var toggleCompleteBtn = taskLi.querySelector('.toggle-complete-btn');
+    var deleteBtn = taskLi.querySelector('.delete-task-btn');
+
+    toggleCompleteBtn.addEventListener('click', function () {
+        toggleTaskComplete(this);
+    });
+
+    deleteBtn.addEventListener('click', function () {
+        deleteTask(this);
+    });
+
+    // Add task to DOM
+    tasksContainer.appendChild(taskLi);
+}
+
+function prepareTaskHTML(title) {
+    return '<div class="input-group">' +
+        '<span class="input-group-btn">' +
+             '<button class="btn btn-default toggle-complete-btn"><i class="fa fa-check"></i></button>'  +
+        '</span>' +
+
+            '<input type="text" class="form-control" placeholder="Task name.." value="' + title + '">' +
+    
+        '<span class="input-group-btn">' +
+            '<button class="btn btn-danger"><i class="fa fa-times"></i></button>' +
+        '</span>' +
+    '</div>';
+}
+
+// Add new task enets
+
+function bindAddTaskEvents() {
+    
+    // On submit
+    newTaskForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var title = this.querySelector('input').value;
+
+        if (title) {
+            addNewTask(title);
+        }
+
+        addNewTask(title);
+    });
+}
---
+++ js/modules/tasks.js
@@ -0,0 +1,6 @@
+var tasks = [
+    'Shopping',
+    'Dentist appointment',
+    'MOT',
+    'Learn JavaScript!'
+];
